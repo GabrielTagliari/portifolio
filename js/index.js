fa3d3a5f6730edd9584428531446f3581f96ef9d
Gabriel Tagliari Rodrigo <gabriel.tagliari93@gmail.com>
2018-06-25 23:12:23 -0300

    Corrigindo problema responsivo layout

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
-$('.navegacao, .me-conheca, .avancar').click(function() {
+var prevScrollpos = window.pageYOffset;
+var cabecalho = document.getElementById("cabecalho");
+
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        cabecalho.classList.add("scrollDown");
+        cabecalho.classList.remove("scrollUp");
+    } else {
+        cabecalho.classList.add("scrollUp"); 
+        cabecalho.classList.remove("scrollDown");               
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+$('.navegacao, .me-conheca').click(function() {
     var id = $(this).attr('id');
     $('html, body').animate({
         scrollTop: ($('#' + id + '.section').offset().top)
     }, 1000);
 });
+
